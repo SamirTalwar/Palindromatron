ba70f3c931134ad479fb1f1df1f216ec881824ed
Samir Talwar <samir@noodlesandwich.com>
2012-02-07 23:58:52 +0000

    Renamed a variable.

--- src/main/java/com/noodlesandwich/palindromatron/model/SimplePalindromes.java
+++ src/main/java/com/noodlesandwich/palindromatron/model/SimplePalindromes.java
@@ -3,8 +3,8 @@ package com.noodlesandwich.palindromatron.model;
 public class SimplePalindromes implements Palindromes {
     @Override
     public boolean verify(final String string) {
-        final String normalisedString = string.toLowerCase().replaceAll("\\W", "");
-        final String reversed = new StringBuffer(normalisedString).reverse().toString();
-        return normalisedString.equals(reversed);
+        final String normalised = string.toLowerCase().replaceAll("\\W", "");
+        final String reversed = new StringBuffer(normalised).reverse().toString();
+        return normalised.equals(reversed);
     }
 }
